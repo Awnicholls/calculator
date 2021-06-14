7d002711b5ddf3aa9fbda6b0ec4ac93e3bde6c7a
awnicholls <awnicholls56@gmail.com>
2021-06-14 19:30:19 +0100

    tidied up comments and fixed display/input fuctionality

--- script.js
+++ script.js
@@ -8,35 +8,33 @@ const decimalButton = document.querySelector("[data-decimal]");
 const numberButton = document.querySelectorAll("[data-number]");
 
 const calculator = {
-  userInput: '0',
+  userInput: "",
   firstOperand: null,
   waitingForSecondOperand: false,
   operator: null,
 };
-function updateDisplay(){
-userInput.textContent = calculator.userInput;
-};
+
+function updateDisplay() {
+  userInput.textContent = calculator.userInput;
+}
 updateDisplay();
-// let firstOperand = "";
-// let secondOperand = "";
-// let operator = null;
-// let waitingForSecondOperand = false;
 
 function add(a, b) {
-    return a + b;
-};
+  return a + b;
+}
 
 function substract(a, b) {
-    return a - b;
-};
+  return a - b;
+}
 
 function multiply(a, b) {
-    return a * b;
-};
+  return a * b;
+}
 
 function divide(a, b) {
-    return a / b;
-};
+  return a / b;
+}
+
 window.addEventListener("keydown", setInput);
 equalsButton.addEventListener("click", handleOperator);
 clearButton.addEventListener("click", clear);
@@ -45,23 +43,23 @@ decimalButton.addEventListener("click", appendDecimal);
 operatorButton.forEach((button) =>
   button.addEventListener("click", () => handleOperator(button.textContent))
 );
+
 function resetScreen() {
   userInput.textContent = "";
   result.textContent = "";
-};
+}
 
 function deleteNumber() {
   userInput.textContent = userInput.textContent.toString().slice(0, -1);
-  calculator.userInput = calculator.userInput.toString().slice(0, -1); 
-  console.log(calculator);}
- 
-
+  calculator.userInput = calculator.userInput.toString().slice(0, -1);
+  console.log(calculator);
+}
 
 function updateResult() {
   result.textContent = calculator.firstOperand;
-};
+}
+
 function clear() {
-  userInput.textContent = "";
   result.textContent = "";
   calculator.userInput = "";
   calculator.firstOperand = null;
@@ -69,104 +67,86 @@ function clear() {
   calculator.operator = null;
   calculator.waitingForSecondOperand = false;
   console.log(calculator);
-};
+  updateDisplay();
+}
 
 numberButton.forEach((button) =>
   button.addEventListener("click", () => appendNumber(button.textContent))
 );
-
-// function appendNumber(number) {
-// if (userInput.textContent === "0") resetScreen();
-// calculator.userInput = userInput === '0' ? number : userInput.textContent += number;
-// };
 
 function appendNumber(number) {
   const { waitingForSecondOperand } = calculator;
 
   if (waitingForSecondOperand === true) {
     resetScreen();
-    calculator.userInput =  userInput.textContent += number;
+    calculator.userInput = userInput.textContent += number;
     calculator.waitingForSecondOperand = false;
   } else {
-    calculator.userInput = calculator.userInput === '0' ? number : calculator.userInput += number;
+    calculator.userInput =
+      calculator.userInput === "0" ? number : (calculator.userInput += number);
   }
   updateDisplay();
   console.log(calculator);
-};
+}
 
+function resetScreen() {
+  userInput.textContent = "";
+}
 
-  function resetScreen() {
-    userInput.textContent = "";
-  };
-  
 function appendDecimal() {
-  // if (calculator.waitingForSecondOperand === true) {
-  // 	calculator.userInput = '0.'
-  //   calculator.waitingForSecondOperand = false;
-  //   return
-  // }
-    if (calculator.userInput === "" || calculator.waitingForSecondOperand === true){
-   	calculator.userInput = '0.'
-     calculator.waitingForSecondOperand = false;
-      calculator.userInput = "0";}
-    if (calculator.userInput.includes(".")) return;
-    calculator.userInput += ".";
-    console.log(calculator);
-    updateDisplay();
-  };
-
-
+  if (
+    calculator.userInput === "" ||
+    calculator.waitingForSecondOperand === true
+  ) {
+    calculator.userInput = "0.";
+    calculator.waitingForSecondOperand = false;
+    calculator.userInput = "0";
+  }
+  if (calculator.userInput.includes(".")) return;
+  calculator.userInput += ".";
+  console.log(calculator);
+  updateDisplay();
+}
 
 function handleOperator(nextOperator) {
-    // Destructure the properties on the calculator object
-    const { firstOperand, userInput, operator } = calculator
-    // `parseFloat` converts the string contents of `displayValue`
-    // to a floating-point number
-    const inputValue = parseFloat(userInput);
-    if (operator && calculator.waitingForSecondOperand)  {
-      calculator.operator = nextOperator;
-      console.log(calculator);
-      return;
-    }
-    // verify that `firstOperand` is null and that the `inputValue`
-    // is not a `NaN` value
-    if (firstOperand === null && !isNaN(inputValue)) {
-      // Update the firstOperand property
-      calculator.firstOperand = inputValue;
-    } else if (operator) {
-      const result = calculate(firstOperand, inputValue, operator);
-      calculator.userInput = `${parseFloat(result.toFixed(7))}`;
-      calculator.firstOperand = result;
-      updateResult();
-    }
-    console.log(calculator);
-
-  
-    calculator.waitingForSecondOperand = true;
+  const { firstOperand, userInput, operator } = calculator;
+  const inputValue = parseFloat(userInput);
+  if (operator && calculator.waitingForSecondOperand) {
     calculator.operator = nextOperator;
-  };
+    console.log(calculator);
+    return;
+  }
+  if (firstOperand === null && !isNaN(inputValue)) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+    calculator.userInput = `${parseFloat(result.toFixed(7))}`;
+    calculator.firstOperand = result;
+    updateResult();
+  }
+  console.log(calculator);
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+}
 
+function calculate(firstOperand, secondOperand, operator) {
+  a = Number(firstOperand);
+  b = Number(secondOperand);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "−":
+      return substract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null, alert("No can do");
+      else return divide(a, b);
+    default:
+      return secondOperand;
+  }
+}
 
-
-  function calculate(firstOperand, secondOperand, operator) {
-    a = Number(firstOperand);
-    b = Number(secondOperand);
-    switch (operator) {
-      case "+":
-        return add(a, b);
-      case "−":
-        return substract(a, b);
-      case "×":
-        return multiply(a, b);
-      case "÷":
-        if (b === 0) 
-         return null, alert("No can do");
-        else return divide(a, b);
-      default:
-        return secondOperand;
-    }
-  };
-  
 function setInput(e) {
   if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
   if (e.key === ".") appendDecimal();
@@ -183,8 +163,3 @@ function convertOperator(keyboardOperator) {
   if (keyboardOperator === "-") return "−";
   if (keyboardOperator === "+") return "+";
 }
-  
-  
-  
-  
-  
